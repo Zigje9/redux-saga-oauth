1427e0ab46f968895a4af9926248828a49fcabef
jekoo <pjkwprn@gmail.com>
2021-09-01 13:24:21 +0900

    feat: add github icon & text

--- redux-saga-oauth/src/components/loginView/githubBox.tsx
+++ redux-saga-oauth/src/components/loginView/githubBox.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Github } from '@styled-icons/boxicons-logos/Github';
 
 const Box = styled.div`
   display: flex;
   width: 35%;
   height: 15%;
-  border: 1px solid black;
+  align-items: center;
+`;
+
+const Icon = styled(Github)`
+  width: 20%;
+  &:hover {
+    cursor: pointer;
+    color: #e0e0e0;
+  }
+`;
+
+const Board = styled.div`
+  width: 80%;
+  height: 80%;
+  border-radius: 30px;
+  background-color: black;
+  color: white;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+    background-color: #e0e0e0;
+    color: black;
+  }
 `;
 
 const GitHubBox: React.FC = () => {
-  return <Box></Box>;
+  return (
+    <Box onClick={() => console.log('here')}>
+      <Icon></Icon>
+      <Board>GitHub 으로 로그인</Board>
+    </Box>
+  );
 };
 
 export default GitHubBox;
