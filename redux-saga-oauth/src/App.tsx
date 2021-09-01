d33e4e3ffda1446ae6f3ce65359df7555b4fc8aa
jekoo <pjkwprn@gmail.com>
2021-09-01 12:44:24 +0900

    feat: add loginView

--- redux-saga-oauth/src/App.tsx
+++ redux-saga-oauth/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './assets/styles/globalStyle';
 import { theme } from './assets/styles/theme';
+import LoginView from './pages/loginView';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <div>Start</div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LoginView} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
---
+++ redux-saga-oauth/src/pages/loginView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoginView: React.FC = () => {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default LoginView;
