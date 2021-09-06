ae6582a230b0621be56a437055551058a0680ccf
jekoo <pjkwprn@gmail.com>
2021-09-06 16:39:22 +0900

    feat: add default user router

--- oauth-server/app.ts
+++ oauth-server/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import userRouter from './src/user/router';
 
 const app = express();
 const port = 5000;
@@ -13,6 +14,8 @@ const corsOptions = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
