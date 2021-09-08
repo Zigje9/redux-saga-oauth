bcce3917e95a489c1e56d9c0aa540743fe4fda3c
jekoo <pjkwprn@gmail.com>
2021-09-08 17:34:41 +0900

    feat: add loading to saga

--- redux-saga-oauth/src/sagas/user.ts
+++ redux-saga-oauth/src/sagas/user.ts
@@ -1,6 +1,7 @@
 import { put, call, fork, take, all } from 'redux-saga/effects';
 import * as type from '../actions/type';
-import * as action from '../actions/user';
+import * as userAction from '../actions/user';
+import * as loadingAction from '../actions/loading';
 import { getAccessToken } from '../api/axios';
 
 interface TokenData {
@@ -14,13 +15,15 @@ function* authenticationSaga(
   stateCode: string,
   site: string,
 ): Generator {
+  yield put(loadingAction.loadingStart(true));
   try {
     const response = yield call(getAccessToken, code, stateCode, site);
     const { data } = response as TokenData;
     const accessToken = data.token;
-    yield put(action.authenticationSuccess(accessToken));
+    yield put(userAction.authenticationSuccess(accessToken));
+    yield put(loadingAction.loadingEnd(false));
   } catch (error) {
-    yield put(action.authenticationFailure());
+    yield put(userAction.authenticationFailure());
     console.log(error);
   }
 }
@@ -29,7 +32,7 @@ function* watchAuthenticationRequestSaga(): Generator {
   while (true) {
     const response = yield take(type.AUTHENTICATION_REQUEST);
     const { code, stateCode, site } = response as ReturnType<
-      typeof action.authenticationRequest
+      typeof userAction.authenticationRequest
     >;
     yield call(authenticationSaga, code, stateCode, site);
   }
