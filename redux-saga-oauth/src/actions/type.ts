4998f624795ecab8dc368b15a2101e84b269b5b8
jekoo <pjkwprn@gmail.com>
2021-09-12 14:20:55 +0900

    feat: add login action type

--- redux-saga-oauth/src/actions/type.ts
+++ redux-saga-oauth/src/actions/type.ts
@@ -3,10 +3,15 @@ export const AUTHENTICATION_REQUEST = 'AUTHENTICATION_REQUEST' as const;
 export const AUTHENTICATION_SUCCESS = 'AUTHENTICATION_SUCCESS' as const;
 export const AUTHENTICATION_FAILURE = 'AUTHENTICATION_FAILURE' as const;
 
+export const LOGIN_REQUEST = 'LOGIN_REQUEST' as const;
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS' as const;
+export const LOGIN_FAILURE = 'LOGIN_FAILURE' as const;
 export interface AuthProps {
   accessToken: string;
   code: string;
   stateCode: string;
+  isLogin: boolean;
+  userId: string;
 }
 
 /* LOADING ACTION TYPE */
